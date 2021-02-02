9f8cc9c0b009e451f32a569112c9f3e0c96d4692
Emily Lumbert <emilylumbert@yahoo.com>
2021-02-02 17:57:41 -0500

    file IO pair pt 2 done

--- 17_FileIO_Writing_out/pair-exercise/dotnet/FindAndReplace/Program.cs
+++ 17_FileIO_Writing_out/pair-exercise/dotnet/FindAndReplace/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace FindAndReplace
 {
@@ -6,7 +11,42 @@ namespace FindAndReplace
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //C:\Users\Student\week04-c-sharp-pairs-team02\16_FileIO_Reading_in\pair-exercise\dotnet\alice.txt
+            
+            //C:\Users\Student\week04-c-sharp-pairs-team02\16_FileIO_Reading_in\pair-exercise\alice.txt
+            Console.WriteLine("Give me a phrase you wanna get rid of");
+            string findPhrase = Console.ReadLine();
+            Console.WriteLine("What should that phrase be instead?");
+            string replace = Console.ReadLine();
+            Console.WriteLine("Alright... now tell me the file we're looking in");
+            string aliceLocation = Console.ReadLine();
+            Console.WriteLine("nooooowww where do you want this file to be?");
+            string output = Console.ReadLine();
+
+            try
+            {
+
+                using (StreamReader sr = new StreamReader(aliceLocation))
+                {
+                    using (StreamWriter sw = new StreamWriter(output, true))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            string replacedLine = line.Replace(findPhrase, replace);
+                            sw.WriteLine(replacedLine);
+
+                        }
+                    }
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message + "Whoops that file already exists there");
+            }
+            Console.WriteLine("we did it");
+
         }
     }
 }
