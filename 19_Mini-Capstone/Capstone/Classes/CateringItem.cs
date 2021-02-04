ff42b4a4b039c2047861a433b6d66aadaa4d410b
Emily Lumbert <emilylumbert@yahoo.com>
2021-02-04 16:56:35 -0500

    Thursday night push

--- 19_Mini-Capstone/Capstone/Classes/CateringItem.cs
+++ 19_Mini-Capstone/Capstone/Classes/CateringItem.cs
@@ -6,7 +6,34 @@ namespace Capstone.Classes
 {
     public class CateringItem
     {
+        //Code|Name|Price|Type
         // This class should contain the definition for one catering item
 
+        int quantity = 50;
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Type { get; set; }
+
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+            set
+            {
+                quantity = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Item code {Code}, {Name}, ${Price}, {Quantity} available";
+
+        }
     }
 }
---
+++ 19_Mini-Capstone/Capstone/Classes/Change.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    public class Change
+    {
+        //Catering catering = new Catering();
+
+
+
+
+        public string GiveChange(Catering catering)
+        {
+            int nickel = 0;
+            int dime = 0;
+            int quarter = 0;
+            int oneDollar = 0;
+            int fiveDollar = 0;
+            int tenDollar = 0;
+            int twentyDollar = 0;
+
+            while (catering.AccountBalance > 0)
+            {
+                if (catering.AccountBalance >= 20)
+                {
+                    twentyDollar++;
+                    catering.AccountBalance -= 20;
+
+                }
+                else if (catering.AccountBalance >= 10)
+                {
+                    tenDollar++;
+                    catering.AccountBalance -= 10;
+                }
+                else if (catering.AccountBalance >= 5)
+                {
+                    fiveDollar++;
+                    catering.AccountBalance -= 5;
+                }
+                else if (catering.AccountBalance >= 1)
+                {
+                    oneDollar++;
+                    catering.AccountBalance -= 1;
+                }
+                else if (catering.AccountBalance >= .25M)
+                {
+                    quarter++;
+                    catering.AccountBalance -= .25M;
+                }
+                else if (catering.AccountBalance >= .10M)
+                {
+                    dime++;
+                    catering.AccountBalance -= .10M;
+                }
+                else if (catering.AccountBalance >= .05M)
+                {
+                    nickel++;
+                    catering.AccountBalance -= .05M;
+                }
+
+            }
+            return $"You were given {twentyDollar} 20's, {tenDollar} 10's, {fiveDollar} 5's, {oneDollar} 1's, {quarter} quarters, {dime} dimes, and {nickel} nickels. Thanks!";
+
+        }
+
+    }
+}
